2e24dc4799b7b33cb610453241d625bb8dd31371
mantisas <mantasbog@gmail.com>
2018-10-26 17:53:06 +0300

    update

--- app/class/CSSIMAGESPRITERS.php
+++ app/class/CSSIMAGESPRITERS.php
@@ -1,3 +1,16 @@
 <?php
+class CSSIMAGESPRITERS {
+    var $inputImageDir;
+    protected $imageObj;
 
+    public function process() {
+        //check input image directory
+        if(!file_exists($this->dir)) die('not set direcotry, or not exsist');
+
+        //get images array
+        $this->imageObj = scandir($this->inputImageDir);
+
+        //create output CSS, export output image
+    }
+}
 ?>
